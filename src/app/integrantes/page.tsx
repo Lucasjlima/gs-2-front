951ee7dbec19b57cfe99c2d120f2f8fdba04a69d
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-05-28 09:16:58 -0300

    compIntegrante

--- src/app/integrantes/page.tsx
+++ src/app/integrantes/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import {FaGithub } from "react-icons/fa";
+import CompIntegrante from "../components/compIntegrante/compIntegrante";
 
 const Integrantes = () => {
 
@@ -12,74 +12,29 @@ const Integrantes = () => {
                     </h1>
 
                     <div>
-                        <article className="article-integrante">
-                            <Image
-                                src="/images/foto-eduardo.jpg"
-                                alt="Foto do integrante Eduardo Osterloh Bindo"
-                                width={500}
-                                height={500}
-                                className="foto-integrante"
-                            />
-                            <h3>
-                                Eduardo Osterloh Bindo
-                            </h3>
-                            <p>RM: 559755</p>
-                            <p>Turma: 1TDSPA</p>
-                            <a
-                                href="https://www.linkedin.com/in/eduardo-osterloh-bindo-500b02268/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="icone-linkedin"
-                            >
-                                <FaLinkedin />
-                            </a>
-                        </article>
+                        <CompIntegrante
+                            nome="Eduardo Osterloh Bindo"
+                            rm="559755"
+                            turma="1TDSPA"
+                            foto="/images/foto-eduardo.jpg"
+                            linkedin="https://www.linkedin.com/in/eduardo-osterloh-bindo-500b02268/"
+                        />
+                        
+                        <CompIntegrante
+                            nome="Lucas José Lima"
+                            rm="561160"
+                            turma="1TDSPA"
+                            foto="/images/foto-lucas.jpg"
+                            linkedin="https://www.linkedin.com/in/lucasjos%C3%A9lima/"
+                        />
 
-                        <article className="article-integrante">
-                            <Image
-                                src="/images/foto-lucas.jpg"
-                                alt="Foto do integrante Lucas José Lima"
-                                width={500}
-                                height={500}
-                                className="foto-integrante"
-                            />
-                            <h3>
-                                Lucas José Lima
-                            </h3>
-                            <p>RM: 561160</p>
-                            <p>Turma: 1TDSPA</p>
-                            <a
-                                href="https://www.linkedin.com/in/lucasjos%C3%A9lima/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="icone-linkedin"
-                            >
-                                <FaLinkedin />
-                            </a>
-                        </article>
-
-                        <article className="article-integrante">
-                            <Image
-                                src="/images/foto-rangel.jpg"
-                                alt="Foto do integrante Rangel Bernardi Jordão"
-                                width={500}
-                                height={500}
-                                className="foto-integrante"
-                            />
-                            <h3>
-                                Rangel Bernardi Jordão
-                            </h3>
-                            <p>RM: 560547</p>
-                            <p>Turma: 1TDSPA</p>
-                            <a
-                                href="https://www.linkedin.com/in/rangel-jord%C3%A3o-819334234/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="icone-linkedin"
-                            >
-                                <FaLinkedin />
-                            </a>
-                        </article>
+                        <CompIntegrante
+                            nome="Rangel Bernardi Jordão"
+                            rm="560547"
+                            turma="1TDSPA"
+                            foto="/images/foto-rangel.jpg"
+                            linkedin="https://www.linkedin.com/in/rangel-bernardi-jord%C3%A3o-819334234/"
+                        />
                     </div>
 
                     <h2 className="text-2xl font-semibold mt-10">
